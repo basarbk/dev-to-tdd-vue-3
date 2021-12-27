7f57f2051dbe2a4eef42f931df2a69aac758cf1d
Basar Buyukkahraman <basarbk@yahoo.com>
2021-12-27 13:11:42 +0300

    Add axios and make api call

--- src/RandomUser.spec.js
+++ src/RandomUser.spec.js
@@ -1,6 +1,9 @@
 import RandomUser from './RandomUser.vue';
 import { render, screen } from '@testing-library/vue';
 import "@testing-library/jest-dom";
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
+
 
 describe('Random User', () => {
   it('has button to load random user', () => {
@@ -9,5 +12,29 @@ describe('Random User', () => {
       name: 'Load Random User'
     });
     expect(loadButton).toBeInTheDocument();
+  });
+  it('displays title, first and lastname of loaded user from randomuser.me', async () => {
+    render(RandomUser);
+    const loadButton = screen.queryByRole('button', {
+      name: 'Load Random User'
+    });
+
+    const mockApiCall = jest.fn().mockResolvedValue({
+      data: {
+        results: [
+          {
+            name: {
+              title: 'Miss',
+              first: 'Jennifer',
+              last: 'Alvarez'
+            }
+          }
+        ]
+      }
+    });
+    axios.get = mockApiCall;
+    userEvent.click(loadButton);
+    const userInfo = await screen.findByText('Miss Jennifer Alvarez');
+    expect(userInfo).toBeInTheDocument();
   });
 });
--- src/RandomUser.vue
+++ src/RandomUser.vue
@@ -1,3 +1,24 @@
 <template>
-  <button>Load Random User</button>
+  <div>
+    <button @click="loadRandomUser">Load Random User</button>
+    <h1 v-if="user">{{user.name.title}} {{user.name.first}} {{user.name.last}}</h1>
+  </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      user: undefined
+    };
+  },
+  methods: {
+    async loadRandomUser(){
+      try {
+        const response = await axios.get('https://randomuser.me/api');
+        this.user = response.data.results[0];
+      } catch (error) {/**/}
+    }
+  }
+};
+</script>
